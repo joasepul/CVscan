53a833fb65c625c90a0e5f399e7569a835c30d25
balagotb <balagotb@gmail.com>
2018-05-26 16:31:55 -0700

    Added default_index.js for vueification

---
+++ static/js/default_index.js
@@ -0,0 +1,36 @@
+// JS for default/index.html
+
+var app = function() {
+
+    var self = {};
+
+    Vue.config.silent = false; // show all warnings
+
+    //Vue functions go here
+
+
+    //Call Vue data and methods here
+    self.vue = new Vue({
+        el: "#vue-div",
+        delimiters: ['${', '}'],
+        unsafeDelimiters: ['!{', '}'],
+        data: {
+
+        },
+        methods: {
+            
+        }
+
+    });
+
+    //Anything else needed goes here
+
+    $("#vue-div").show();
+    return self;
+};
+
+var APP = null;
+
+// This will make everything accessible from the js console;
+// for instance, self.x above would be accessible as APP.x
+jQuery(function(){APP = app();});
