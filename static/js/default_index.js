68581cc203cd54e9ee723943d590b2bd365cb3dc
aemurill <aemurill@ucsc.edu>
2018-06-10 15:13:51 -0700

    Removed video.js code

--- static/js/default_index.js
+++ static/js/default_index.js
@@ -20,80 +20,6 @@ var app = function() {
             (navigator.userAgent.indexOf('IEMobile') !== -1);
     }
 
-    /* Video Selector */
-    self.initVideo = function(){
-        if (!hasGetUserMedia()) {
-            alert('getUserMedia() is not supported by your browser');
-        }
-        self.vue.videoSelect = document.querySelector('select#videoSource');
-        /* Video Element */
-        self.vue.videoElement = document.getElementById("video");
-        // self.vue.videoElement = document.createElement("video");
-        // document.getElementById("videoSourceSelect").appendChild(self.vue.videoElement);
-        if(self.vue.videoElement != null){
-            self.vue.videoElement.autoplay = true;
-            self.vue.videoElement.playsinline = true;
-            navigator.mediaDevices.enumerateDevices().then(gotDevices).then(self.getStream).catch(handleError);
-            /* video feed handling */
-            console.log("video initialized");
-        }
-    };
-
-
-    /* Helper Functions */
-    function gotDevices(deviceInfos) {
-      console.log('gotDevices');
-      for (var i = 0; i !== deviceInfos.length; ++i) {
-        var deviceInfo = deviceInfos[i];
-        var option = document.createElement('option');
-        option.value = deviceInfo.deviceId;
-        if (deviceInfo.kind === 'videoinput') {
-          option.text = deviceInfo.label || 'camera ' +
-            (self.vue.videoSelect.length + 1);
-          self.vue.videoSelect.appendChild(option);
-          console.log('Video Device Found', deviceInfo);
-        } else {
-          console.log('Found one other kind of source/device: ', deviceInfo);
-        }
-      }
-    }
-
-
-    //return selected feed
-    self.getStream = function() {
-      console.log('getStream');
-      if (window.stream) {
-        window.stream.getTracks().forEach(function (track) {
-          track.stop();
-        });
-
-      }
-      var constraints = {
-        audio: false,
-        video: {
-          deviceId: { exact: self.vue.videoSelect.value }
-        }
-      };
-      //display feed
-      navigator.mediaDevices.getUserMedia(constraints).
-        then(gotStream).catch(handleError);
-    };
-
-    //set feed
-    function gotStream(stream) {
-      console.log('gotStream');
-      window.stream = stream; // make stream available to console
-      self.vue.videoElement.srcObject = stream;
-      self.vue.videoElement.onloadedmetadata = function(){
-          self.vue.videoWidth = this.videoWidth;
-          self.vue.videoHeight = this.videoHeight;
-          console.log(this.videoWidth);
-          console.log(this.videoHeight);
-
-      }
-    }
-
-
     function handleError(error) {
       alert('Error: ' + error);
       console.log('Error: ', error);
@@ -103,24 +29,6 @@ var app = function() {
         console.log('resetPhoto');
         $("#mainState1").hide();
         $("#mainState0").show();
-    };
-
-    /* On button click, create video snapshot */
-    self.takeScreenshot = function() {
-        console.log('takeScreenshot');
-        $("#mainState0").hide();
-        $("#mainState1").show();
-        self.vue.drawbutton = document.querySelector('#draw-button');
-        self.vue.canvas = document.querySelector('#imgcanvas');
-        self.vue.canvas.width = self.vue.videoWidth;
-        self.vue.canvas.height = self.vue.videoHeight;
-        self.vue.canvas.getContext('2d').drawImage(self.vue.videoElement, 0, 0);
-        //Other browsers will fall back to image/png
-        // self.vue.img.src = canvas.toDataURL('image/webp');
-        self.vue.img.src = self.vue.canvas.toDataURL('image/png'); //create snapshot of canvas
-        self.vue.dataURL = self.vue.img.src;
-        //clear_CanvasState();
-        self.init_coord_draw();
     };
 
     self.openFile = function(event) {
@@ -213,13 +121,12 @@ var app = function() {
       ctx.fillStyle = this.fill;
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
-      ctx.lineWidth = 3 * self.vue.rescaleConst;
+      ctx.lineWidth = 3* self.vue.rescaleConst;
       ctx.strokeStyle = this.strokeStyle;
       ctx.stroke();
     };
 
     /* Determine if a point is inside the shape's bounds */
-    ///CONSIDER: INCREASE DETECTION RADIUS OR ACTUAL SIZE?
     Shape.prototype.contains = function(mx, my) {
       // is the mouse withinn the area of the circle?
       return (Math.pow(mx - this.x, 2) + Math.pow(my - this.y, 2) <= Math.pow(this.r, 2));
@@ -400,6 +307,7 @@ var app = function() {
         }
 
         ctx.beginPath();
+        ctx.lineWidth = ctx.lineWidth/3;
         ctx.moveTo(shapes[0].x, shapes[0].y);
         ctx.lineTo(shapes[2].x, shapes[2].y);
         ctx.lineTo(shapes[3].x, shapes[3].y);
@@ -611,11 +519,7 @@ var app = function() {
         unsafeDelimiters: ['!{', '}'],
         data: {
             logged_in: false,
-            //archive_mode: false,
             imagelist: [],
-            main_state: 0,
-            videoSelect: null,
-            videoElement: null,
             img: new Image,
             canvas: null,
             myCanvasState: null, //CanvasState
@@ -629,9 +533,6 @@ var app = function() {
         },
         methods: {
             pdf_test: self.pdf_test,
-            initVideo: self.initVideo,
-            getStream: self.getStream,
-            takeScreenshot: self.takeScreenshot,
             init_coord_draw: self.init_coord_draw,
             sendToServer: self.sendToServer,
             post_button: self.post_button,
@@ -653,7 +554,6 @@ var app = function() {
 
     //Anything else needed goes here
     setupFakeUsers();
-    self.initVideo();
     $("#vue-div").show();
     return self;
 };
