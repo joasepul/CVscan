1759f35b5484da439b625034340d409cc72a0575
balagotb <balagotb@gmail.com>
2018-06-11 10:50:26 -0700

    trying download

--- controllers/api.py
+++ controllers/api.py
@@ -82,7 +82,7 @@ def add_pdf():
 
 @auth.requires_login()
 def get_pdfs():
-    print('getting users')
+    print('getting pdfs')
     auth_id = auth.user.id
     pdfList = []
     row = db(db.user_documents.created_by == auth.user.id).select(orderby=~db.user_documents.created_on)
@@ -94,7 +94,7 @@ def get_pdfs():
             id = r.id,
         )
         pdfList.append(t)
-    print('got users')
+    print('got pdfs')
     return response.json(dict(
         pdfList = pdfList,
     ))
